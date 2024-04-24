4cbcacf65662215d6a6fc292dc503d68b7847759
Yuin <cxy856@uowmail.edu.au>
2024-04-24 12:17:06 +1000

    added points list

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,6 +5,7 @@ function Player({defaultName, onNameChange}){
 
     const [point, setPoint] = useState();
     const [totalPoints, setTotalPoints] = useState(0);
+    const [pointList, setPointList] = useState([]);
 
     const handlePointInput = (e) => {
         const points = parseInt(e.target.value);
@@ -12,6 +13,7 @@ function Player({defaultName, onNameChange}){
     }
 
     const handleAddButton = () => {
+        setPointList([...pointList, point]);
         setTotalPoints(totalPoints + point);
         setPoint(0);
     }
@@ -20,6 +22,10 @@ function Player({defaultName, onNameChange}){
         <div className='PlayerContainer'>
             <input type='text' value={defaultName} onChange={(e)=>onNameChange(e.target.value)} className='PlayerName'/>
             <p className='TotalPoints'>Total Points: {totalPoints}</p>
+            <div>
+                {pointList.map((element, index) => (
+                <span className='IndividualPoints' key={index}>{element}</span>))}
+            </div>
             <input type='number' value={point} onChange={handlePointInput} placeholder='Insert points' className='PointsInput'/>
             <button className='AddButton' onClick={handleAddButton}>Add</button>
         </div>
